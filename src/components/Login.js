19c0b5c7c29e2e55eaab571e003b8094a69a9777
Chris Lau <chrislyc1991@gmail.com>
2021-07-16 23:22:19 -0600

    login form is working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,61 @@
-import React from "react";
-
+import React, {useState} from "react";
+import * as yup from "yup";
+import {useForm} from "../hooks/useForm";
+import axios from "axios";
+const schema = yup.object().shape({
+  username: yup.string().required("Username or Password not valid"),
+  password: yup.string().required("Username or Password not valid")
+});
+const initialValues = {
+  username:"",
+  password:""
+};
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-
-  const error = "";
+  const [formValues,setFormValues,validate] = useForm(initialValues,schema);
+  const [error,setError] = useState("");
   //replace with error state
-
+  const handleChange=(e)=>{
+    const{name, value} = e.target;
+    setFormValues({...formValues,[name]:value});
+  }
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    const [formErrors,isValid] = await validate();
+    if(isValid){
+      setError("");
+      axios.post("http://localhost:5000/api/login",formValues)
+      .then((res)=>{
+        localStorage.setItem("token",res.data.payload);
+      })
+      .catch((err)=>{
+        console.log({err});
+      })
+      // make api call
+      // localStorage.setItem("token",)
+    }
+    else{
+      setError(formErrors[Object.keys(formErrors).find(key=>formErrors[key]!=="")]);
+    }
+  };
+  const {username, password} = formValues;
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
         <h2>Build login form here</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            User Name
+            <input id="username" name="username" type="text" value={username} onChange={handleChange} />
+          </label>
+          <label>
+            Password
+            <input id="password" name="password" type="password" value={password} onChange={handleChange} />
+          </label>
+          <button id="submit" type="submit">Submit</button>
+        </form>
       </div>
 
       <p id="error" className="error">{error}</p>
