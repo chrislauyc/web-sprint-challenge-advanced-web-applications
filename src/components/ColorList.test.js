c636b3003770b6d7b89bf9cafa9769e1c8b12e96
Chris Lau <chrislyc1991@gmail.com>
2021-07-18 01:57:28 -0600

    finished colorlist.test

--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -3,12 +3,53 @@ import MutationObserver from 'mutationobserver-shim';
 
 import { render, screen} from "@testing-library/react";
 import ColorList from './ColorList';
+import Color from './Color';
+import MockEditMenu from './EditMenu';
+jest.mock("./EditMenu")
+
 
 test("Renders an empty list of colors without errors", () => {
+    render(<ColorList colors={[]}/>);
 });
 
 test("Renders a list of colors without errors", () => {
+    const colors = [
+        {
+          color: "aliceblue",
+          code: {
+            hex: "#f0f8ff",
+          },
+          id: 1,
+        },
+        {
+          color: "limegreen",
+          code: {
+            hex: "#99ddbc",
+          },
+          id: 2,
+        },];
+    render(<ColorList colors={colors}/>);
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+    const colors = [
+        {
+          color: "aliceblue",
+          code: {
+            hex: "#f0f8ff",
+          },
+          id: 1,
+        },
+        {
+          color: "limegreen",
+          code: {
+            hex: "#99ddbc",
+          },
+          id: 2,
+        },];
+    MockEditMenu.mockImplementation(()=><div>mock edit menu</div>)
+    const {rerender} = render(<ColorList colors={colors} editing={true}/>);
+    expect(MockEditMenu.call.length).toBe(1);
+    rerender(<ColorList colors={colors} editing={false}/>);
+    expect(MockEditMenu.call.length).toBe(1);
 });
